26433cb65c9b98c56fa0f4e96d242e2cb89a4866
Marko Grešak <marko@gresak.io>
2015-04-09 02:52:56 +0200

    add class MainFrame, used as frame by MainWindow

--- src/main/java/mm/display/MainWindow.java
+++ src/main/java/mm/display/MainWindow.java
@@ -3,16 +3,63 @@ package mm.display;
 import mm.structures.ColorField;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 public class MainWindow {
 
+    private MainFrame window;
+
     public MainWindow(ColorField field){
-        FieldPanel fieldPanel = new FieldPanel(field);
-        JFrame window = new JFrame();
+
+        this.window = new MainFrame("Vojna Barv", field);
         window.setSize(500, 522);
-        fieldPanel.setSize(500, 522);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.add(fieldPanel);
         window.setVisible(true);
+    }
+
+    public void repaint() {
+       window.repaint();
+    }
+
+    public void setField(ColorField field) {
+        window.setField(field);
+    }
+}
+
+class MainFrame extends JFrame implements ComponentListener {
+
+    private FieldPanel fieldPanel;
+
+    MainFrame(String title, ColorField field) throws HeadlessException {
+        super(title);
+        this.fieldPanel = new FieldPanel(field);
+        fieldPanel.setSize(this.getSize());
+        this.add(fieldPanel);
+    }
+
+    public void setField(ColorField field) {
+        fieldPanel.setField(field);
+    }
+
+    @Override
+    public void componentResized(ComponentEvent e) {
+        fieldPanel.setSize(this.getSize());
+    }
+
+    @Override
+    public void componentMoved(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentShown(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentHidden(ComponentEvent e) {
+
     }
 }
