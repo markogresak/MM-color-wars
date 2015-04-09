d0906a5c3e159a7c31a898af6dcda0c6c7f0945c
Marko Grešak <marko@gresak.io>
2015-04-09 04:55:44 +0200

    use all colors, constant for size of field

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,22 +7,25 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
+
+        final int N = 100;
+
         ColorPixel[] colors = new ColorPixel[]{
                 new ColorPixel(new Color(124, 7, 142)),
                 new ColorPixel(new Color(236, 74, 72)),
                 new ColorPixel(new Color(255, 252, 88)),
                 new ColorPixel(new Color(31, 162, 209)),
-//                new ColorPixel(new Color(128, 207, 12)),
-//                new ColorPixel(new Color(71, 34, 69)),
-//                new ColorPixel(new Color(1, 153, 138)),
-//                new ColorPixel(new Color(206, 23, 54)),
-//                new ColorPixel(new Color(247, 88, 48)),
-//                new ColorPixel(new Color(12, 8, 65))
+                new ColorPixel(new Color(128, 207, 12)),
+                new ColorPixel(new Color(71, 34, 69)),
+                new ColorPixel(new Color(1, 153, 138)),
+                new ColorPixel(new Color(206, 23, 54)),
+                new ColorPixel(new Color(247, 88, 48)),
+                new ColorPixel(new Color(12, 8, 65))
         };
 
         long start = System.nanoTime();
-        ColorField cf = ColorField.GenerateField(50, colors);
-        System.out.printf("generiranje polja: %d\n", System.nanoTime() - start);
+        ColorField cf = ColorField.GenerateField(N, colors);
+        System.out.printf("generiranje polja %d x %d: %.3fs\n", N, N, (System.nanoTime() - start) / 1e9);
 
         MainWindow window = new MainWindow(cf);
 
