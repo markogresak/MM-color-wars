c1e862ad6a420824097777eaedd6d7a02540500d
Marko Grešak <marko@gresak.io>
2015-04-14 00:43:38 +0200

    warn if missing npm dependancies files

--- webserver/index.js
+++ webserver/index.js
@@ -1,8 +1,12 @@
-var port = process.env.PORT || 3000;
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/public'));
-require('http').createServer(app)
+try {
+  var port = process.env.PORT || 3000;
+  var express = require('express');
+  var app = express();
+  app.use(express.static(__dirname + '/public'));
+  require('http').createServer(app)
   .listen(port, function () {
     console.log('Server listening at port %d', port);
   });
+} catch (e) {
+  console.log('Unable to start server. Make sure to run "npm install".');
+}
