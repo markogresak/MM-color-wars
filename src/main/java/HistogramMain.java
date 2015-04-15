9c83a97f76b0bdf82968efb41cfa4c0c2c6bdb39
Marko Grešak <marko@gresak.io>
2015-04-15 21:24:22 +0200

    fix histogram

--- src/main/java/HistogramMain.java
+++ src/main/java/HistogramMain.java
@@ -13,14 +13,16 @@ import java.util.Random;
 public class HistogramMain {
 
     public static void main(String[] args) throws IOException {
-        final int[] Ns = new int[]{5, 10, 25, 50};
-        final int[] Ts = new int[]{1, 5, 10, 25, 50};
-        final int[] samples = new int[]{10, 50, 100, 250, 500, 1000};
+
+//        final int[] Ns = new int[]{5, 10, 25};
+//        final int[] Ts = new int[]{1, 5, 10, 25, 50};
+//        final int[] samples = new int[]{10, 50, 100, 250, 500};
+        final int[] Ns = new int[]{25};
+        final int[] Ts = new int[]{1, 5, 10, 25, 50, 100, 250, 500, 1000, 2000, 3000, 4000, 5000};
+        final int[] samples = new int[]{1000};
         for (int s : samples) {
             for (int n : Ns) {
-                for (int t : Ts) {
-                    new Thread(new SamplingTask(n, s, t)).start();
-                }
+                new Thread(new SamplingTask(n, s, Ts)).start();
             }
         }
     }
@@ -28,9 +30,11 @@ public class HistogramMain {
 
 class SamplingTask implements Runnable {
 
-    int N, SAMPLES, HISTOGRAM_T;
+    int N;
+    int SAMPLES;
+    int[] HISTOGRAM_T;
 
-    public SamplingTask(int N, int SAMPLES, int HISTOGRAM_T) {
+    public SamplingTask(int N, int SAMPLES, int[] HISTOGRAM_T) {
         this.N = N;
         this.SAMPLES = SAMPLES;
         this.HISTOGRAM_T = HISTOGRAM_T;
@@ -42,16 +46,16 @@ class SamplingTask implements Runnable {
         final Random random = new Random("VojnaBarv".hashCode());
 
         ColorPixel[] colors = new ColorPixel[]{
-                new ColorPixel(new Color(124, 7, 142), 0.3),
-                new ColorPixel(new Color(236, 74, 72), 0.05),
-                new ColorPixel(new Color(255, 252, 88), 0.05),
-                new ColorPixel(new Color(31, 162, 209), 0.1),
-                new ColorPixel(new Color(128, 207, 12), 0.1),
-                new ColorPixel(new Color(71, 34, 69), 0.05),
-                new ColorPixel(new Color(1, 153, 138), 0.125),
-                new ColorPixel(new Color(206, 23, 54), 0.075),
-                new ColorPixel(new Color(247, 88, 48), 0.05),
-                new ColorPixel(new Color(12, 8, 65), 0.1)
+                new ColorPixel(new Color(124, 7, 142), 0.25),
+                new ColorPixel(new Color(236, 74, 72), 0.75 ),
+//                new ColorPixel(new Color(255, 252, 88), 0.05),
+//                new ColorPixel(new Color(31, 162, 209), 0.3),
+//                new ColorPixel(new Color(128, 207, 12), 0.1),
+//                new ColorPixel(new Color(71, 34, 69), 0.05),
+//                new ColorPixel(new Color(1, 153, 138), 0.125),
+//                new ColorPixel(new Color(206, 23, 54), 0.075),
+//                new ColorPixel(new Color(247, 88, 48), 0.05),
+//                new ColorPixel(new Color(12, 8, 65), 0.1)
         };
         ColorPixel.setCodes(colors);
 
@@ -59,64 +63,93 @@ class SamplingTask implements Runnable {
         ColorField cf = initialCf;
 
         long sTotal = System.nanoTime();
-        ArrayList<Double>[] samples = new ArrayList[SAMPLES];
+        double[][] samples = new double[HISTOGRAM_T.length][SAMPLES];
         for (int i = 0; i < SAMPLES; i++) {
-            samples[i] = new ArrayList<>();
+            int tIndex = 0;
             long st = System.nanoTime();
             int iterations = 0;
-            while (!cf.isAllSame()) {
+            while (tIndex < HISTOGRAM_T.length && !cf.isAllSame()) {
                 cf = cf.updateNeighbours();
-                if (iterations % HISTOGRAM_T == 0) {
-                    samples[i].add(cf.getColorPercentage(colors[0]));
+                if (iterations == HISTOGRAM_T[tIndex]) {
+                    samples[tIndex++][i] = cf.getColorPercentage(colors[0]);
                 }
                 iterations++;
             }
-            System.out.printf("samples-%d-%dx%d-t%d: %.3fs, i: %d\n",
-                    SAMPLES, N, N, HISTOGRAM_T, (System.nanoTime() - st) / 1e9, iterations);
+            if(i % 100 == 0) {
+                System.out.printf("%d / %d\n", i + 1, SAMPLES);
+            }
             cf = initialCf;
         }
         System.out.printf("done, time: %.3fs\n", (System.nanoTime() - sTotal) / 1e9);
 
         long startSampling = System.nanoTime();
 
-        ArrayList<Double> avgSamples = new ArrayList<>();
-        boolean samplesExist = true;
-        int sampleI = 0;
-        while (samplesExist) {
-            double sum = 0;
-            int count = 0;
-            for (int i = 0; i < SAMPLES; i++) {
-                try {
-                    sum += samples[i].get(sampleI);
-                    count++;
-                } catch (Exception e) {
-                    // Number doesn't exist, don't to anything.
+        for (int i = 0; i < HISTOGRAM_T.length; i++) {
+
+            int t = HISTOGRAM_T[i];
+            ArrayList<HistogramPoint> usedSamples = new ArrayList<>();
+
+            for (int j = 0; j < SAMPLES; j++) {
+                double xt = samples[i][j];
+                if (xt > 0.0 && xt < 1) {
+                    double yt = Math.log(xt / (1 - xt));
+                    yt = Math.round(yt * 100.0) / 100.0;
+                    HistogramPoint p = new HistogramPoint(yt, 1);
+                    int ytIndex = usedSamples.indexOf(p);
+                    if (ytIndex >= 0) {
+                        usedSamples.get(ytIndex).addY();
+                    } else {
+                        usedSamples.add(p);
+                    }
                 }
             }
-            samplesExist = count > 0;
-            if (samplesExist) {
-                double xt = sum / ((double) count);
-                avgSamples.add(Math.log(1 + (xt / (1 - xt))));
+
+            String[] samplesJSON = new String[usedSamples.size()];
+            for (int j = 0; j < samplesJSON.length; j++) {
+                samplesJSON[j] = usedSamples.get(j).toString();
             }
-            sampleI++;
+            String json = Arrays.toString(samplesJSON);
+
+            File file = new File(String.format("./webserver/public/data/samples-%d-%dx%d-t%d.json", SAMPLES, N, N, t));
+            file.getParentFile().mkdirs();
+
+            try {
+                Files.write(Paths.get(file.getPath()), json.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.printf("done sampling samples-%d-%dx%d-t%d, time: %.3f\n",
+                    SAMPLES, N, N, t, (System.nanoTime() - startSampling) / 1e9);
+        }
+    }
+}
+
+class HistogramPoint {
+
+    double x;
+    int y;
+
+    public HistogramPoint(double x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void addY() {
+        y++;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{\"x\": %.10f, \"y\": %d}", x, y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || !(o instanceof HistogramPoint)) {
+            return false;
         }
 
-        String[] samplesJSON = new String[avgSamples.size()];
-        for (int i = 0; i < samplesJSON.length; i++) {
-            samplesJSON[i] = String.format("{\"x\": %d, \"y\": %.3f}", i * HISTOGRAM_T, avgSamples.get(i));
-        }
-        String json = Arrays.toString(samplesJSON);
-
-        File file = new File(String.format("./webserver/public/data/samples-%d-%dx%d-t%d.json", SAMPLES, N, N, HISTOGRAM_T));
-        file.getParentFile().mkdirs();
-
-        try {
-            Files.write(Paths.get(file.getPath()), json.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.printf("done sampling samples-%d-%dx%d-t%d, time: %.3f\n",
-                SAMPLES, N, N, HISTOGRAM_T, (System.nanoTime() - startSampling) / 1e9);
+        return ((HistogramPoint) o).x == this.x;
     }
 }
