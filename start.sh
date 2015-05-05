6926a17cc752d37efaa599143d851655b41c6b1f
Marko Grešak <marko@gresak.io>
2015-05-05 16:38:51 +0200

    change checking for command to function

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
+function check_commands {
+  if [[ -n $1 ]]; then
+    command -v $1 >/dev/null 2>&1 || { echo >&2 "Command '$1' is requred for this script to run correctly."; exit 1; }
+    shift
+    check_commands $@
+  fi
+}
+
 # Check for maven command.
-command -v mvn >/dev/null 2>&1 || { echo >&2 "Command 'mvn' (maven) is requred for this script to run correctly."; exit 1; }
+check_commands mvn
 
 # Compile and run the project.
 mvn compile test
